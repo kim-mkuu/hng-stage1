c75fc64a834d3cd7be6232c255d212b882a6728c
Kim Mkuu <kimukuphilip10@gmail.com>
2025-10-22 18:30:57 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RESTful API string analyzer
+
+## Build a RESTful API service that analyzes strings and stores their computed properties
---
+++ strings/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Models creation
+class string(models.Model):
+    value = models.TextField(unique=True)
+    length = models.IntegerField()
+    is_palindrome = models.BooleanField()
+    unique_characters = models.IntegerField()
+    word_count = models.IntegerField()
+    sha256_hash = models.CharField(max_length=64, unique=True, primary_key=True)
+    character_frequency_map = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'strings'
+
+    def __str__(self):
+        return self.value
