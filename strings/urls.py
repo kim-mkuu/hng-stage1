3a54beb4aeeec96f340542c06cf385a6c67f5720
Kim Mkuu <kimukuphilip10@gmail.com>
2025-10-24 01:41:09 +0300

    fix: Fix URL routing patterns

--- strings/urls.py
+++ strings/urls.py
@@ -1,5 +1,4 @@
-
-from django.urls import path
+from django.urls import path, re_path
 from .views import (
     StringAPIView,
     StringDetailDeleteView,
@@ -7,7 +6,10 @@ from .views import (
 )
 
 urlpatterns = [
+    
     path('strings/filter-by-natural-language', StringNaturalLanguageFilterView.as_view(), name='string-nl-filter'),
-    path('strings/<path:string_value>', StringDetailDeleteView.as_view(), name='string-detail-delete'),
+   
+    re_path(r'^strings/(?P<string_value>.+)$', StringDetailDeleteView.as_view(), name='string-detail-delete'),
+    
     path('strings', StringAPIView.as_view(), name='string-api'),
 ]
